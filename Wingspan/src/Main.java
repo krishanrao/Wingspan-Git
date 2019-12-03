a72ca9c7387fea12fed1d19a996a5bc93e05ef16
vShroff <42785832+vShroff@users.noreply.github.com>
2019-12-02 20:53:10 -0800

    Add files via upload

--- Wingspan/src/Main.java
+++ Wingspan/src/Main.java
@@ -1,4 +1,4 @@
-package connect4;
+package Wing;
 
 import java.util.Scanner;
 
@@ -7,56 +7,43 @@ public class Main {
 	
 	public static void main(String[] args)
 	{  
-		//Initializing 2 players and a board
-		Player p1= new Player(1);
-		Player p2= new Player(2);
+		
 		GameBoard g = new GameBoard();
-		
-		Player currentPlayer = p1;// setting first player to be the current player
+		int currentPlayer = 1;
 		int colChosen=0;
-		
-		g.displayBoard(); //displaying board in the beginning
-		
+		g.displayBoard();
 		boolean gameNotOver =true; // will be false when game is over
 		while(gameNotOver)
 		{
-		System.out.println("Player " + currentPlayer.getNum() + " What col would you like to choose b/w 0-6");
-		System.out.println("Press 7 to Quit");
-		//add input validation later
-		Scanner sc = new Scanner(System.in);
-		colChosen= sc.nextInt();
-		
-		
-
-		if(colChosen ==7)
-			gameNotOver = false;
-		 
-		else if( colChosen >=0 && colChosen <=6) //If player choses a col num
-		{
+			System.out.println("Player " + currentPlayer + " What col would you like to choose b/w 0-6");
+			System.out.println("Press 7 to Quit");
+			//add input validation later
+			Scanner sc = new Scanner(System.in);
+			colChosen= sc.nextInt();
 			
-			g.traversalVoid(colChosen, currentPlayer); //pass the colNum into traversal
-			// check win
-			g.displayBoard(); //display board
-			currentPlayer= toggle(currentPlayer, p1, p2); //switch to other player
+			if(colChosen ==7)
+				gameNotOver = false;
+			else if( colChosen >=0 && colChosen <=6) //If player choses a col num
+			{
+				
+				g.traversalVoid(colChosen, currentPlayer); //pass the colNum into traversal
+				// check win
+				g.displayBoard(); //display board
+				if(currentPlayer ==1)
+				{
+					currentPlayer = 2;
+				}
+				else
+				{
+					currentPlayer = 1;
+				}
+				
+			}
 			
-		}
-		
-		
 		}
 		System.out.println("Game Over!");
 		g.displayBoard();
 		
-	}
-	//function to change from one player to another
-public static Player toggle(Player Person, Player person1, Player person2) { 
-		
-		if (person1.getNum() == Person.getNum()) {
-			return person2;
-		} 
-		else {
-		return person1;
-	}
-	
 	}
 
 }
